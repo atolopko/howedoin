1c5f9657b7bfb896a3a3df574895e07c4170f4fa
Andrew Tolopko <coder1@tolopko.com>
2013-01-13 21:01:25 -0500

    virgin Rails-generated skeleton app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howedoin::Application.config.secret_token = 'b819da315626364c644455e8e9965114da6d9a44cf9025ce605ac119fdc9ffe3fcfdf63affc1e9971790a6261e95b709aed2b4fbe381a9f3917c905eae00ca1b'

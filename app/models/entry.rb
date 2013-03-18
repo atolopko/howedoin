de7950deb68f5fd2ec8224cf31d4d77d26abae43
Andrew Tolopko <git@tolopko.com>
2013-03-17 21:55:42 -0400

    - added User model - Entry creation working

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,9 @@
 class Entry < ActiveRecord::Base
   self.table_name = 'entry'
 
+  attr_accessible :account, :user, :amount
+
   belongs_to :txn, primary_key: 'trans_id'
   belongs_to :account, foreign_key: 'acct_id', readonly: true
+  belongs_to :user, readonly: true
 end

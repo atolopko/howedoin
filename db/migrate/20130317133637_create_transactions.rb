25963bdf5ded44689b2bf9fde0a62c974685c969
Andrew Tolopko <git@tolopko.com>
2013-03-24 09:23:29 -0400

    removed all migrations, as these are not applicable in the legacy branch

--- db/migrate/20130317133637_create_transactions.rb
+++
@@ -1,8 +0,0 @@
-class CreateTransactions < ActiveRecord::Migration
-  def change
-    create_table :transactions do |t|
-
-      t.timestamps
-    end
-  end
-end
--- db/migrate/20130317133650_create_payees.rb
+++
@@ -1,8 +0,0 @@
-class CreatePayees < ActiveRecord::Migration
-  def change
-    create_table :payees do |t|
-
-      t.timestamps
-    end
-  end
-end

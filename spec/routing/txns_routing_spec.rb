1bfaa3fc2be7a4a7823d7920e298e4aa2df86982
Andrew Tolopko <git@tolopko.com>
2013-04-07 09:38:27 -0400

    fixed broken generated tests

--- spec/routing/txns_routing_spec.rb
+++ spec/routing/txns_routing_spec.rb
@@ -4,31 +4,31 @@ describe TxnsController do
   describe "routing" do
 
     it "routes to #index" do
-      get("/txns").should route_to("txns#index")
+      get("/txns").should route_to("txns#index", :format => :json)
     end
 
     it "routes to #new" do
-      get("/txns/new").should route_to("txns#new")
+      get("/txns/new").should route_to("txns#new", :format => :json)
     end
 
     it "routes to #show" do
-      get("/txns/1").should route_to("txns#show", :id => "1")
+      get("/txns/1").should route_to("txns#show", :id => "1", :format => :json)
     end
 
     it "routes to #edit" do
-      get("/txns/1/edit").should route_to("txns#edit", :id => "1")
+      get("/txns/1/edit").should route_to("txns#edit", :id => "1", :format => :json)
     end
 
     it "routes to #create" do
-      post("/txns").should route_to("txns#create")
+      post("/txns").should route_to("txns#create", :format => :json)
     end
 
     it "routes to #update" do
-      put("/txns/1").should route_to("txns#update", :id => "1")
+      put("/txns/1").should route_to("txns#update", :id => "1", :format => :json)
     end
 
     it "routes to #destroy" do
-      delete("/txns/1").should route_to("txns#destroy", :id => "1")
+      delete("/txns/1").should route_to("txns#destroy", :id => "1", :format => :json)
     end
 
   end

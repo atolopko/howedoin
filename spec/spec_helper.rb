748c7cdc5f4e9d353f683bb227704706024ea26d
Andrew Tolopko <atolopko@fiksu.com>
2013-05-26 17:29:49 -0400

    added Zeus support

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+# require 'rspec/autorun' # causes Zeus to run specs twice [https://github.com/burke/zeus/issues/180]
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.

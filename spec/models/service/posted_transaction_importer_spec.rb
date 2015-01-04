b26b98b37f2b800da07fb1abf76ab4b7c19a6486
Andrew Tolopko <github@tolopko.com>
2015-01-04 10:17:27 -0500

    added tests for import of duplicate posted transactions

--- spec/models/service/posted_transaction_importer_spec.rb
+++ spec/models/service/posted_transaction_importer_spec.rb
@@ -3,19 +3,8 @@ require 'spec_helper'
 module Service
   describe PostedTransactionImporter do
 
-    let(:importer) { PostedTransactionImporter.new(records, associated_account) }
-    let(:records) {
-      [{ reference_identifier: "record1" },
-       { reference_identifier: "record2" },
-       { reference_identifier: "record3" }]
-    }
-    let(:input_file) { StringIO.new(posted_txns_json) }
-    let(:posted_txns_json) { MultiJson.dump(records) }
-    let!(:associated_account) { FactoryGirl.create(:account, name: 'account', acct_type_val: 'liability') }
-    let!(:unassigned_account) { FactoryGirl.create(:account, name: 'unassigned', acct_type_val: 'expense') }
-    let!(:user) { FactoryGirl.create(:user) }
-
-    before do
+    def build_importer(records, account)
+      importer = PostedTransactionImporter.new(records, associated_account)
       def importer.populate(pt, r)
         # make a valid PostedTransaction
         pt.sale_date = Date.new(2014, 1, 1)
@@ -27,7 +16,20 @@ module Service
         pt.category = 'category'
         pt.person = 'person'
       end
+      importer
     end
+
+    let(:importer) { build_importer(records, associated_account) }
+    let(:records) {
+      [{ reference_identifier: "record1" },
+       { reference_identifier: "record2" },
+       { reference_identifier: "record3" }]
+    }
+    let(:input_file) { StringIO.new(posted_txns_json) }
+    let(:posted_txns_json) { MultiJson.dump(records) }
+    let!(:associated_account) { FactoryGirl.create(:account, name: 'account', acct_type_val: 'liability') }
+    let!(:unassigned_account) { FactoryGirl.create(:account, name: 'unassigned', acct_type_val: 'expense') }
+    let!(:user) { FactoryGirl.create(:user) }
 
     shared_examples "persisted posted transactions" do
       it "persists all transactions" do
@@ -52,47 +54,26 @@ module Service
 
     describe "#import" do
 
-      describe "valid posted transactions without existing transactions" do
+      describe "valid posted transactions" do
         it_should_behave_like "persisted posted transactions"
-
-        # it "creates associated Txn" do
-        #   expect(PostedTransaction.first.txn.date).to eq Date.new(2014, 1, 17)
-        # end
-
-        # it "creates associated Txn entries" do
-        #   expect(PostedTransaction.first.txn.entries[0].attributes.symbolize_keys).
-        #     to include({acct_id: associated_account.id,
-        #                  user_id: user.id,
-        #                  amount: -0.99,
-        #                  memo: 'NEW YORK TIMES DIGITAL 100001 NY',
-        #                  num: 'xyz123'})
-        #   expect(PostedTransaction.first.txn.entries[1].attributes.symbolize_keys).
-        #     to include({acct_id: unassigned_account.id,
-        #                  user_id: user.id,
-        #                  amount: 0.99,
-        #                  memo: nil,
-        #                  num: nil})
-        # end
       end
 
-      # # describe "valid posted transactions with existing transactions" do
-      # #   let!(:existing_txn) { FactoryGirl.create(:txn,
-      # #                                            date: Date.new(2014, 1, 17),
-      # #                                            amount: 0.99,
-      # #                                            from_account: associated_account) }
+      describe "duplicate posted transactions" do
+        before do
+          build_importer(records, associated_account).import
+        end
 
-      # #   before do
-      # #     PostedTransactionImporter.new(posted_txns).import
-      # #   end
+        it "does not import the duplicate posted transaction" do
+          expect { importer.import }.to_not change { PostedTransaction.count }.from(3)
+        end
 
-      # #   it_should_behave_like "persisted posted transactions"
+        it "reports duplication errors" do
+          importer.import
+          expect(importer.errors).to have(3).errors_on(:reference_identifier)
+        end
+      end
 
-      # #   it "associates with existing Txn" do
-      # #     expect(PostedTransaction.first.txn).to eq existing_txn
-      # #   end
-      # # end
-
-      describe "invalid transactions" do
+      describe "invalid posted transactions" do
         before do
           # force PostedTransaction to be invalid
           allow(importer).to receive(:populate) do |pt, r|
@@ -109,10 +90,10 @@ module Service
           expect(PostedTransaction.count).to eq 0
         end
 
-        it "does not persist any Txns" do
-          importer.import
-          expect(Txn.count).to eq 0
-        end
+        # it "does not persist any Txns" do
+        #   importer.import
+        #   expect(Txn.count).to eq 0
+        # end
 
         it "provides errors in returned posted transactions" do
           importer.import

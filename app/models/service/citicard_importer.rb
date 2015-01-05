73327366dea2610aa39dfef2e32f2f08384c7f73
Andrew Tolopko <github@tolopko.com>
2015-01-05 07:28:29 -0500

    added type_identifier attr to CitiCardImporter

--- app/models/service/citicard_importer.rb
+++ app/models/service/citicard_importer.rb
@@ -5,7 +5,7 @@ module Service
   class CiticardImporter < PostedTransactionImporter
 
     def initialize(posted_txns_csv_io)
-      csv = CSV.new(posted_txns_csv_io, headers: [:post_date, :amt, :memo])
+      csv = CSV.new(posted_txns_csv_io, headers: [:post_date, :amt, :memo, :type], skip_blanks: true)
       posted_txns = csv.readlines.map { |r| r.to_hash }
       super(posted_txns, associated_account)
     end
@@ -16,9 +16,11 @@ module Service
       pt.post_date = Date.strptime r[:post_date], "%Y-%m-%d"
       pt.amount = BigDecimal.new(r[:amt].gsub(/[$,]/, '')) if r[:amt]
       pt.memo = r[:memo]
+      pt.type_identifier = r[:type]
     end
     
     def associated_account
+      # TODO: this should come from a config file
       Account.where("name like 'Citibank MasterCard%'").first
     end
 

25a8e673c27af0e9d64ce1859a823572cc84fef1
Andrew Tolopko <github@tolopko.com>
2015-01-18 16:17:49 -0500

    moved account determination to caller

--- spec/models/service/citicard_importer_spec.rb
+++ spec/models/service/citicard_importer_spec.rb
@@ -10,8 +10,8 @@ module Service
       CSV
     }
     let(:input_file) { StringIO.new(posted_txns_data) }
-    let!(:associated_account) { FactoryGirl.create(:account, name: 'Citibank MasterCard', acct_type_val: 'liability') }
-    let(:importer) { CiticardImporter.new(input_file) }
+    let(:account) { FactoryGirl.create(:account, name: 'Citibank MasterCard', acct_type_val: 'liability') }
+    let(:importer) { CiticardImporter.new(input_file, account) }
 
     describe "#import" do
       describe "valid posted transactions without existing transactions" do
@@ -19,12 +19,12 @@ module Service
         it "persists data correctly" do
           importer.import
           expect(PostedTransaction.all.map { |pt| pt.attributes.values_at('account_id', 'post_date', 'amount', 'memo') }).
-            to include([associated_account.id,
+            to include([account.id,
                         Date.new(2014, 12, 1),
                         BigDecimal("94.72"),
 #                         "type_identifier" => "2",
                          "TRADER JOE'S #999  QPS SPRINGFIELD   MA"],
-                       [associated_account.id,
+                       [account.id,
                         Date.new(2014, 12, 2),
                         BigDecimal("21.31"),
 #                         "type_identifier" => "2",

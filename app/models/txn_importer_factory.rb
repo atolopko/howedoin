72f787236205d7823706f4ada33cc420949dcd7a
Andrew Tolopko <github@tolopko.com>
2016-01-06 11:14:09 -0500

    TxnImportererFactory creation command

--- app/models/txn_importer_factory.rb
+++ app/models/txn_importer_factory.rb
@@ -6,7 +6,25 @@ class TxnImporterFactory < ActiveRecord::Base
   belongs_to :from_account, class_name: 'Account'
   belongs_to :to_account, class_name: 'Account'
 
-  validates :memo_regexp, :user, :payee, :from_account, :to_account, presence: true
+  validates :memo_regexp, :user, :from_account, :to_account, presence: true
+
+  # Creates TxnImporterFactory where attributes for associated models
+  # may be may a model instance, id, or an SQL wildcard string
+  # matching the name of a unique instance
+  def self.smart_create!(attrs)
+    to_account = TxnBuilder.resolve_model(attrs[:to_account], Account)
+    from_account = TxnBuilder.resolve_model(attrs[:from_account], Account)
+    payee = TxnBuilder.resolve_model(attrs[:payee], Payee)
+    user = TxnBuilder.resolve_model(attrs[:user], User, :nickname)
+    TxnImporterFactory.
+      new(memo_regexp: attrs[:memo_regexp],
+          min_amount: nil,
+          max_amount: nil,
+          to_account: to_account,
+          from_account: from_account,
+          user: user,
+          payee: payee)
+  end
 
 end
 

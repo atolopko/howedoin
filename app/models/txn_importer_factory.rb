aae60535248d72d5bdf6acf658636e1565bc5855
Andrew Tolopko <github@tolopko.com>
2016-02-19 07:50:40 -0500

    Txn importer factory creation script
    Finish implementation

--- app/models/txn_importer_factory.rb
+++ app/models/txn_importer_factory.rb
@@ -11,19 +11,19 @@ class TxnImporterFactory < ActiveRecord::Base
   # Creates TxnImporterFactory where attributes for associated models
   # may be may a model instance, id, or an SQL wildcard string
   # matching the name of a unique instance
-  def self.smart_create(attrs)
+  def self.smart_create!(attrs)
     to_account = TxnBuilder.resolve_model(attrs[:to_account], Account)
     from_account = TxnBuilder.resolve_model(attrs[:from_account], Account)
     payee = TxnBuilder.resolve_model(attrs[:payee], Payee)
     user = TxnBuilder.resolve_model(attrs[:user], User, :nickname)
     TxnImporterFactory.
-      create(memo_regexp: attrs[:memo_regexp],
-          min_amount: attrs[:min_amount],
-          max_amount: attrs[:max_amount],
-          to_account: to_account,
-          from_account: from_account,
-          user: user,
-          payee: payee)
+      create!(memo_regexp: attrs[:memo_regexp],
+              min_amount: attrs[:min_amount],
+              max_amount: attrs[:max_amount],
+              to_account: to_account,
+              from_account: from_account,
+              user: user,
+              payee: payee)
   end
 
 end
